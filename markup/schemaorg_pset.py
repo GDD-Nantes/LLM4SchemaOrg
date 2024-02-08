30915b294097a9f31549bf35e31fceb1c81f07cf
Minh-Hoang DANG <minhhoangdang@hotmail.com>
2024-02-08 16:14:42 +0100

    Quick fix

--- markup/schemaorg_pset.py
+++ markup/schemaorg_pset.py
@@ -167,7 +167,7 @@ def extract(h, d, feature, stratum_sample_size, fpc, explain, quantile, clean):
             n_total = int(row["stratum_sample_size"])
             progress_bar = tqdm(total=n_total, desc="Sampling...")
 
-            logger.debug("BEFORE", len(index_list), len(urls))
+            logger.debug(f"BEFORE {len(index_list)} {len(urls)}")
 
             while len(index_list) > 0 and len(urls) < n_total:
                 np.random.seed(RANDOM_SEED)
@@ -226,7 +226,7 @@ def extract(h, d, feature, stratum_sample_size, fpc, explain, quantile, clean):
                     else:
                         url_blocklist[domain] += 1
             
-            logger.debug("AFTER", len(index_list), len(urls))
+            logger.debug(f"AFTER {len(index_list)} {len(urls)}")
 
             row["unit_classes"] = " ".join([ "|".join(cs) for cs in classes ])
             row["unit_index"] = " ".join([str(x) for x in indexes])
